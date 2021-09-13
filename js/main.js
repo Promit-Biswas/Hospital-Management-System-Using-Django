9fb1c5a6ea7773f6476e0009f705069230ae1baa
Promit <promit776648@gmail.com>
2021-09-13 13:01:52 +0600

    First stage

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+function showpass() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+
+var y = document.getElementById("myInput1");
+  if (y.type === "password") {
+    y.type = "text";
+  } else {
+    y.type = "password";
+  }
+
+
+}
+
+function regbtn() {
+
+var password = document.getElementById("myInput")
+  , confirm_password = document.getElementById("myInput1");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Password doesn't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+}
